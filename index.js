7a8a506e44377ac215b6e54525ef3ab8b634ef4d
Rick <rick329mo@gmail.com>
2022-09-06 08:53:27 -0500

    added new list functionality

---
+++ index.js
@@ -0,0 +1,43 @@
+const listsContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
+
+let lists = [{
+    id: 1,
+    name: 'name'
+}, {
+    id: 2,
+    name: 'to-do'
+}]
+
+newListForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const listName = newListInput.value
+    if (listName == null || listName === '') return
+    const list = createList(listName)
+    newListInput.value = null
+    lists.push(list)
+    render()
+})
+
+function createList(name) {
+   return { id: Date.now().toString(), name: name, tasks: [] }
+}
+
+function render() {
+    clearElement(listsContainer)
+    lists.forEach(list => {
+        const listElement =document.createElement('li')
+        listElement.dataset.listId = list.id
+        listElement.classList.add("list-neame")
+        listElement.innerText = list.name
+        listsContainer.appendChild(listElement)
+    })
+}
+
+function clearElement(element) {
+    while (element.firstChild) {
+        element.removeChild(element.firstChild)
+    }
+}
+render()
