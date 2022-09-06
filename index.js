2365898a9b7107dd72da27af5349230cd1f62ef6
Rick <rick329mo@gmail.com>
2022-09-06 09:35:17 -0500

    added functionality for the delete button

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
+const deleteListButton = document.querySelector('[data-delete-list-button]')
 
 const LOCAL_STORAGE_LIST_KEY = 'task.list'
 const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
@@ -12,6 +13,12 @@ listsContainer.addEventListener('click', e => {
       selectedListId = e.target.dataset.listId 
       saveAndRender()
     }
+})
+
+deleteListButton.addEventListener('click', e => {
+    lists = lists.filter(list => list.id !== selectedListId)
+    selectedListId = null
+    saveAndRender()
 })
 
 newListForm.addEventListener('submit', e => {
