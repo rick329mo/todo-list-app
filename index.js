6f3a6ac3a05ad2fa6ed0873fd3783da05096eb61
Rick <rick329mo@gmail.com>
2022-09-06 09:26:57 -0500

    added localstorage functionality for the app

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const listsContainer = document.querySelector('[data-lists]')
 const newListForm = document.querySelector('[data-new-list-form]')
 const newListInput = document.querySelector('[data-new-list-input]')
 
-let lists = [{
-    id: 1,
-    name: 'name'
-}, {
-    id: 2,
-    name: 'to-do'
-}]
+const LOCAL_STORAGE_LIST_KEY = 'task.list'
+const LOCAL_STORAGE_SELECTED_LIST_ID_KEY = 'task.selectedListId'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || []
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY)
+
+listsContainer.addEventListener('click', e => {
+    if (e.target.tagName.toLowerCase() === "li") {
+      selectedListId = e.target.dataset.listId 
+      saveAndRender()
+    }
+})
 
 newListForm.addEventListener('submit', e => {
     e.preventDefault()
@@ -17,11 +21,21 @@ newListForm.addEventListener('submit', e => {
     const list = createList(listName)
     newListInput.value = null
     lists.push(list)
-    render()
+    saveAndRender()
 })
 
 function createList(name) {
    return { id: Date.now().toString(), name: name, tasks: [] }
+}
+
+function saveAndRender() {
+    save()
+    render()
+}
+
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_ID_KEY, selectedListId)
 }
 
 function render() {
@@ -31,6 +45,9 @@ function render() {
         listElement.dataset.listId = list.id
         listElement.classList.add("list-neame")
         listElement.innerText = list.name
+        if (list.id === selectedListId) { 
+            listElement.classList.add('active-list')
+        }
         listsContainer.appendChild(listElement)
     })
 }
